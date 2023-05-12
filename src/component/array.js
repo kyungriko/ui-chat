eabf7727f0d202ad9b393840228baf8129be52cb
이지현 <jhyi@inswave.com>
2023-05-12 10:46:29 +0900

    새창 열기, 다운로드 아이콘 추가 및 기능 추가

--- src/component/array.js
+++ src/component/array.js
@@ -13,22 +13,23 @@ const Array = (e) => {
   let [isLoading, setIsLoading] = useState(false);
   const scrollRef = useRef();
 
-  useEffect(() => {  // 로딩 완료 여부 확인
+  useEffect(() => {
+    // 로딩 완료 여부 확인
     console.log("reslistchanged:" + isLoading);
-    
+
     setIsLoading((check) => false);
     scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-  }, [resList])
+  }, [resList]);
 
-  useEffect(() => {  // 로딩 완료 여부 확인
+  useEffect(() => {
+    // 로딩 완료 여부 확인
     console.log("listchanged:" + isLoading);
 
-    if(list.length > 0) {
+    if (list.length > 0) {
       setIsLoading((check) => true);
     }
     scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-    
-  }, [list])
+  }, [list]);
 
   const onChange = (e) => {
     setInputText(e.target.value);
@@ -45,6 +46,8 @@ const Array = (e) => {
           question: tempList[len - 1].question,
           answer: str.message,
           code: str.code,
+          script: str.script,
+          url: str.url,
           qId: tempList[len - 1].qId,
           aId: tempList[len - 1].aId,
         };
@@ -57,6 +60,8 @@ const Array = (e) => {
           question: str,
           answer: "",
           code: "",
+          script: "",
+          url: "",
           qId: "q" + counter,
           aId: "a" + counter,
         };
@@ -68,10 +73,9 @@ const Array = (e) => {
   const onClick = (e) => {
     const newList = list.concat(inputText);
     setList(newList);
-    setCounter(counter+1);
+    setCounter(counter + 1);
     updateChatHistory("question", inputText);
     setInputText("");
-    
 
     let req = { message: inputText };
 
@@ -81,7 +85,9 @@ const Array = (e) => {
       .then((result) => {
         const newList = resList.concat(result.data.message);
         setResList(newList);
-        setIsLoading((check) => {return false;});
+        setIsLoading((check) => {
+          return false;
+        });
         updateChatHistory("answer", result.data);
       })
       .catch(() => {
@@ -93,33 +99,41 @@ const Array = (e) => {
     <div className="chatBox">
       <div className="chatHeader"></div>
       <div className="chat">
-        <div className="chatTitle">WebSquare5 SP6의 예제가 궁금하세요?
-          <Loading dynamicClass={isLoading === true ? '':'loadingStop'}></Loading>
+        <div className="chatTitle">
+          WebSquare5 SP6의 예제가 궁금하세요?
+          <Loading
+            dynamicClass={isLoading === true ? "" : "loadingStop"}
+          ></Loading>
         </div>
         <div className="chatMain" ref={scrollRef}>
-        {list.map((data, i) => {
-          return (
-            <div className="chatContainer" key={i}>
-              <div className="questionContainer" key={chatList[i].qId}>
-                <Chat name={data} id={chatList[i].qId + "question_list"} />
-              </div>
+          {list.map((data, i) => {
+            return (
+              <div className="chatContainer" key={i}>
+                <div className="questionContainer" key={chatList[i].qId}>
+                  <Chat name={data} id={chatList[i].qId + "question_list"} />
+                </div>
 
-              
-              <div className="answerContainer" key={chatList[i].aId}>
-                {resList.map((el, idx) => {
-                  if (idx === i) {
-                    return (
-                      <Answer name={el} id={chatList[i].aId + "answer_list"} seq={idx} answerCode={chatList[i].code}/>
-                    );
-                  }
-                })}
-                ;
+                <div className="answerContainer" key={chatList[i].aId}>
+                  {resList.map((el, idx) => {
+                    if (idx === i) {
+                      return (
+                        <Answer
+                          name={el}
+                          id={chatList[i].aId + "answer_list"}
+                          seq={idx}
+                          answerCode={chatList[i].code}
+                          answerScript={chatList[i].script}
+                          answerUrl={chatList[i].url}
+                        />
+                      );
+                    }
+                  })}
+                  ;
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
-        
       </div>
       <div className="send">
         <div className="sendForm">
